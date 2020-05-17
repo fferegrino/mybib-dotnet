4c0d5c37d7885e00c6e2a0f3a2e40ea1bfad727f
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-05-17 12:06:53 +0100

    add values controller

--- MyBib.WebApi/Controllers/ValuesController.cs
+++ MyBib.WebApi/Controllers/ValuesController.cs
@@ -1,15 +1,24 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
+using MyBib.WebApi.Services;
+
 namespace MyBib.WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class ValuesController : ControllerBase
     {
+        IPaymentService paymentService;
+
+        public ValuesController(IPaymentService paymentService)
+        {
+            this.paymentService = paymentService;
+        }
+
         public string Get()
         {
-            return string.Empty;
+            return paymentService.GetMessage();
         }
     }
 }
