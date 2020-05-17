0e66e7f9e4c59a874ed3800bfe555fac4c6878de
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-05-17 11:27:05 +0100

    overwrite middleware

--- MyBib.WebApi/Startup.cs
+++ MyBib.WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +32,10 @@ namespace MyBib.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Run(async context =>{
+                await context.Response.WriteAsync("Hello world!");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
