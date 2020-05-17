93ce6abe015436dfb2991024881a7c01b12c7569
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-05-17 11:53:39 +0100

    register payment service transient

--- MyBib.WebApi/Startup.cs
+++ MyBib.WebApi/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using MyBib.WebApi.Services;
+
 namespace MyBib.WebApi
 {
     public class Startup
@@ -26,6 +28,7 @@ namespace MyBib.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPaymentService, PaymentService>();
             services.AddControllers();
         }
 
